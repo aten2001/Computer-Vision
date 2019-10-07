18a800fc59fc0b59e7a59230bf23ec0baa5d1eef
Sebastian <seb.hollister@gatech.edu>
2019-10-06 22:09:58 -0400

    d33p

--- ML4T_2019Fall/19fall_defeat_learners/defeat_learners/gen_data.py
+++ ML4T_2019Fall/19fall_defeat_learners/defeat_learners/gen_data.py
@@ -31,18 +31,19 @@ import math
 # this function should return a dataset (X and Y) that will work  		   	  			  	 		  		  		    	 		 		   		 		  
 # better for linear regression than decision trees  		   	  			  	 		  		  		    	 		 		   		 		  
 def best4LinReg(seed=1489683273):  		   	  			  	 		  		  		    	 		 		   		 		  
-    np.random.seed(seed)  		   	  			  	 		  		  		    	 		 		   		 		  
-    X = np.zeros((100,2))  		   	  			  	 		  		  		    	 		 		   		 		  
-    Y = np.random.random(size = (100,))*200-100  		   	  			  	 		  		  		    	 		 		   		 		  
-    # Here's is an example of creating a Y from randomly generated  		   	  			  	 		  		  		    	 		 		   		 		  
-    # X with multiple columns  		   	  			  	 		  		  		    	 		 		   		 		  
-    # Y = X[:,0] + np.sin(X[:,1]) + X[:,2]**2 + X[:,3]**3  		   	  			  	 		  		  		    	 		 		   		 		  
+    np.random.seed(seed)
+    samples = 10
+    cols = 2
+    X = np.random.normal(size=(samples, cols))
+    Y = 0.5*(X[:, 0] + X[:,0]) + 2	  			  	 		  		  		    	 		 		   		 		  
     return X, Y  		   	  			  	 		  		  		    	 		 		   		 		  
   		   	  			  	 		  		  		    	 		 		   		 		  
 def best4DT(seed=1489683273):  		   	  			  	 		  		  		    	 		 		   		 		  
     np.random.seed(seed)  		   	  			  	 		  		  		    	 		 		   		 		  
-    X = np.zeros((100,2))  		   	  			  	 		  		  		    	 		 		   		 		  
-    Y = np.random.random(size = (100,))*200-100  		   	  			  	 		  		  		    	 		 		   		 		  
+    samples = 10
+    cols = 2
+    X = np.random.normal(size=(samples, cols))
+    Y = np.exp((X[:, 0] + X[:,0]) ** 3) 	 		  		  		    	 		 		   		 		  
     return X, Y  		   	  			  	 		  		  		    	 		 		   		 		  
   		   	  			  	 		  		  		    	 		 		   		 		  
 def author():  		   	  			  	 		  		  		    	 		 		   		 		  
